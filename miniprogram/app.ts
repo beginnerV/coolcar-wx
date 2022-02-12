59de26ec8af5b5f90093cc3ce069c3f4abd8ed38
xhb <1284746504@qq.com>
2022-02-12 20:05:30 +0800

    token请求封装

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,4 +1,6 @@
 import camelcaseKeys from "camelcase-keys"
+import { auth } from "./service/proto_gen/auth/auth_pb"
+import { rental } from "./service/proto_gen/rental/rental_pb"
 import { coolcar } from "./service/proto_gen/trip_pb"
 
 // app.ts
@@ -10,26 +12,48 @@ App<IAppOption>({
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
 
-    wx.request({
-      url:'http://localhost:8080/trip/trip123',
-      method:'GET',
-      success:res=>{
-        const getTripResp = coolcar.GetTripResponse.fromObject(camelcaseKeys(res.data as Object,{
-          deep:true,
-        }))
-        console.log(222, getTripResp.trip)
-        console.log(333,coolcar.TripStatus[getTripResp.trip?.status!])
-        // const getTripResp = res.data
-        // getTripResp
-      },
-      fail:console.error
-    })
+    // wx.request({
+    //   url:'http://localhost:8080/trip/trip123',
+    //   method:'GET',
+    //   success:res=>{
+    //     const getTripResp = coolcar.GetTripResponse.fromObject(camelcaseKeys(res.data as Object,{
+    //       deep:true,
+    //     }))
+    //     console.log(222, getTripResp.trip)
+    //     console.log(333,coolcar.TripStatus[getTripResp.trip?.status!])
+    //     // const getTripResp = res.data
+    //     // getTripResp
+    //   },
+    //   fail:console.error
+    // })
 
     // 登录
     wx.login({
       success: res => {
-        console.log(res.code)
+        console.log(1111,res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url:'http://localhost:8080/v1/auth/login',
+          method:'POST',
+          data:{
+            code:res.code,
+          } as auth.v1.ILoginRequest,
+          success:res =>{
+            const loginResp: auth.v1.ILoginResponse = auth.v1.LoginResponse.fromObject(camelcaseKeys(res.data as Object))
+            console.log("登陆",loginResp)
+            wx.request({
+              url:'http://localhost:8080/v1/trip',
+              method:'POST',
+              data:{
+                start:'abc',
+              } as rental.v1.ICreateTripRequest,
+              header:{
+                authorization:"Bearer " + loginResp.accessToken
+              }
+            })
+          },
+          fail:console.error
+        })
       },
     })
   },
